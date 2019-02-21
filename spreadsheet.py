cb776f3a7ca8febadab44d5fb203c39a966feff2
evilbullet <zpn333@gmail.com>
2019-02-21 02:06:20 +0200

    make a table

--- spreadsheet.py
+++ spreadsheet.py
@@ -2,12 +2,31 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime, timedelta
 from operator import itemgetter
+import workdays
+from collections import Counter
 
 # test doc --->
 # https://docs.google.com/spreadsheets/d/1MvLGkcfl5msJEb8DwQXjYaq8HVTi6cJk_JbzIKQAjrA/edit#gid=716963912
 
-summary_header = ['feature','test', 'author', 'comments']
+summary_header = ['Title', 'Assignee', 'PR', 'Finished On', 'Days per test', 'Status', 'Merged', 'feature 0']
+
 cols_count = len(summary_header)
+
+g_docs = {# 'tset 1': '1MvLGkcfl5msJEb8DwQXjYaq8HVTi6cJk_JbzIKQAjrA',
+    'feature 0': '1kViBTBc90DPMnWHZ_WZghcalHcJmkASnA975zmmLPsM',
+
+    'feature 1':'1A9TzAkFXSoT_CAQe6twAn2wYL_1nDTiNyjhh8faZ4r8',
+    'feature 2':'10BMNqsqAkRLZwXZRdxKXVVWBnjG_i2_HZxrSpTtn5x0',
+    'feature 3':'1RwtIusKuKtJJBnjFKt_zbSmpB2XPtNhF1SdZU7pyQxU',
+    'feature 4':'1FVuedc19h17SSWpO4tHZaekBnqnu8Lr-IynZ1YLyXgI',
+    'feature 5':'1sABAOwlE4QpK5MEn6NFLkXqb4Tns3ApFN6l24Y4YF2g',
+    'feature 6':'1Uy-g5HQ2RCQFKNH3lbXKljJTvRRXw_ly7XU-1yps6l8',
+    'feature 7':'1lO5lnsfbl9htgLXfKwa5rmQeGs6cKbe34W367pm8uns',
+    'feature 8':'1ATYskL9uByTr7EP09P77DnTeErS4mz8aNPf3e-dFVT8',
+    'feature 9':'1JXcsUm9OzluzYNsaRSaA_lEz_xhJFH_UNrma6unNIyc',
+    'feature 10':'1cvQpiRr4sWqD_dOmnPvGsRVeVHC_GCylJq_bBzvVlk0',
+    'feature 11': '1esuRzV-u6bjDf2dF7E0J_PFdvvirBhkcvsskyIbr-iM'
+}
 
 def today_check(date):
     DATE_RANGE_START = datetime(day=1, month=1, year=2016)
@@ -30,12 +49,9 @@ def insert_week_headers(test_date):
     start_of_week = date_obj - timedelta(days=date_obj.weekday())  # Monday
     end_of_week = start_of_week + timedelta(days=6)  # Sunday
 
-    arr = ['Week - ' + week_number,
+    return ('Week - ' + week_number, ['Week - ' + week_number,
             'Week start - end: ' + start_of_week.strftime('%Y/%m/%d') +
-            ' - ' + end_of_week.strftime('%Y/%m/%d'), '' , '']
-    return ('Week - ' + week_number,['Week - ' + week_number,
-            'Week start - end: ' + start_of_week.strftime('%Y/%m/%d') +
-            ' - ' + end_of_week.strftime('%Y/%m/%d'), '' , ''])
+            ' - ' + end_of_week.strftime('%Y/%m/%d'), '' , '','','','',''])
 
 def make_datetime(lst):
     if lst[6] == '' or lst[6] == 'Started On'or lst[6]=='Finished On':
@@ -53,36 +69,8 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-g_docs_finished = {
-'feature 1':'1DGdnyxgjKgZ1mN3DFqeFxcOQ9nOp_FQFg-6pR8xbWqs',
-'feature 2':'192huiwOlSMtzi-nBftwdCwPvzhZTdhznqbK46g-jKQk',
-'feature 3':'1Hnzfjur1rqse7gecHMj9s_VQw8-hct6gl-VqgdQj5XE',
-'feature 4':'1PJkKI9zvjBT8etEmrQjqkn9GEBlvUTC--gl-MPX1o64',
-'feature 5':'1aq_gcD2vrtIClhhZVteMT62sRHjE_UXX95Hl6LnQSz8',
-'feature 6':'1XDCESv6trwAgoul1X3IE7l-fCh3dc43eFrcX3Qfo1_g'
-}
-g_docs =    {
-    # 'tset 1': '1MvLGkcfl5msJEb8DwQXjYaq8HVTi6cJk_JbzIKQAjrA',
-'feature 1':'1cvQpiRr4sWqD_dOmnPvGsRVeVHC_GCylJq_bBzvVlk0',
-# 'feature 2':'1kViBTBc90DPMnWHZ_WZghcalHcJmkASnA975zmmLPsM',
-# 'feature 3':'1A9TzAkFXSoT_CAQe6twAn2wYL_1nDTiNyjhh8faZ4r8',
-# 'feature 4':'10BMNqsqAkRLZwXZRdxKXVVWBnjG_i2_HZxrSpTtn5x0',
-# 'feature 5':'1RwtIusKuKtJJBnjFKt_zbSmpB2XPtNhF1SdZU7pyQxU',
-# 'feature 6':'1FVuedc19h17SSWpO4tHZaekBnqnu8Lr-IynZ1YLyXgI',
-# 'feature 7':'1sABAOwlE4QpK5MEn6NFLkXqb4Tns3ApFN6l24Y4YF2g',
-# 'feature 8':'1Uy-g5HQ2RCQFKNH3lbXKljJTvRRXw_ly7XU-1yps6l8',
-# 'feature 9':'1lO5lnsfbl9htgLXfKwa5rmQeGs6cKbe34W367pm8uns',
-# 'feature 10':'1ATYskL9uByTr7EP09P77DnTeErS4mz8aNPf3e-dFVT8',
-# 'feature 11':'1JXcsUm9OzluzYNsaRSaA_lEz_xhJFH_UNrma6unNIyc',
-'feature 12':'1esuRzV-u6bjDf2dF7E0J_PFdvvirBhkcvsskyIbr-iM'
-            }
-
-summary_sheet = client.open('new_summary test').worksheet('all_new')
-
 def collect_all_docs():
     summary_list = []
-    stat_line = []
-    sheet_lines = []
 
     for feature_name, gd_key in g_docs.items():
         sheet_lines = client.open_by_key(gd_key).worksheet('Automation progress').get_all_values()
@@ -93,54 +81,89 @@ def collect_all_docs():
         summary_list = summary_list + stat_lines
     summary_list = sorted(summary_list, key=make_datetime)
     print('total lines in doc: ', str(len(summary_list)))
+    keys = summary_list[0]
+    del(summary_list[0])
 
-worksheet = client.open_by_key(
-    '1cvQpiRr4sWqD_dOmnPvGsRVeVHC_GCylJq_bBzvVlk0').worksheet(
-    'Automation progress')
-all_rows = worksheet.get_all_records()
+    dict_data = [dict(zip(keys, l)) for l in summary_list]
+    # print(dict_data )
+    return dict_data
 
+g_docs_finished = {
+'feature 1':'1DGdnyxgjKgZ1mN3DFqeFxcOQ9nOp_FQFg-6pR8xbWqs',
+'feature 2':'192huiwOlSMtzi-nBftwdCwPvzhZTdhznqbK46g-jKQk',
+'feature 3':'1Hnzfjur1rqse7gecHMj9s_VQw8-hct6gl-VqgdQj5XE',
+'feature 4':'1PJkKI9zvjBT8etEmrQjqkn9GEBlvUTC--gl-MPX1o64',
+'feature 5':'1aq_gcD2vrtIClhhZVteMT62sRHjE_UXX95Hl6LnQSz8',
+'feature 6':'1XDCESv6trwAgoul1X3IE7l-fCh3dc43eFrcX3Qfo1_g'
+}
+
+summary_sheet = client.open('new_summary test').worksheet('all_new')
+all_rows = collect_all_docs()
 performances = []
-processed_rows = []
-keys = ['Title', 'Assignee', 'PR', 'Finished On' ]
-header = ['Title', 'Assignee', 'PR', 'Finished On']
+print(all_rows)
+
 for idx, row in enumerate(all_rows):
-    if row['Status'] is not '' :
-        keys = ('Title', 'Assignee', 'PR', 'Finished On' )
+    if row['Status'] is not '' and row['Finished On'] is not '' and row['Finished On'] is not 'Finished On':
+        keys = ('Title', 'Assignee', 'PR', 'Finished On', 'Days per test', 'Status', 'Merged', 'feature 0' )
+        try:
+            start_date = datetime.strptime(row['Finished On'] , "%d/%m/%Y")
+        except:
+            print(row['Finished On'], row['feature 0'])
+            continue
+        end_date = datetime.strptime(row['Started On'] , "%d/%m/%Y")
+
+        row.update({'Days per test': workdays.networkdays(end_date, start_date)})
         # Create new Dict with only keys from list (Drop extra)
         new_row = {key: row[key] for key in keys}
+        print(row['Assignee'], row['Days per test'])
         performances.append(new_row)
+
+authors  = [d.get('Assignee') for d in performances]
+authors = set(authors)
+# create dict from two lists d = {k:v for k,v in zip(L1,L2)}
+stats_keys = ['Avg days per test', 'Longest test', 'Shortest test', 'Total tests']
+print(authors)
 
 data = sorted(performances, key=itemgetter('Finished On'))
 
 def add_weeks(data):
     out = []
-    x = 'Week - 05 2019'
-    x1 = ['Week - 05 2019']
     for idx, row in enumerate(data):
-        if row['Finished On'] is not '':
-            keys = ('Title', 'Assignee', 'PR', 'Finished On')
+        if row['Finished On'] is not '' :
+            keys = ('Title', 'Assignee', 'PR',  'Finished On',  'Days per test', 'Status', 'Merged', 'feature 0' )
             # Create new Dict with only keys from list (Drop extra)
             new_row = {key: row[key] for key in keys}
-            # if not out.count(insert_week_headers(new_row.get('Finished On'))[0]):
             if not out.count(insert_week_headers(new_row.get('Finished On'))[0]):
                 out.extend(insert_week_headers(new_row.get('Finished On'))[1])
-            # print(insert_week_headers(new_row.get('Finished On'))[0])
-            # print(out.count(x))
-            # print(out.count(x1))
-            # print(insert_week_headers(new_row.get('Finished On'))[0])
             for key in new_row:
                 out.append(new_row[key])
-            # out.append(new_row)
+
             # TODO: avg time
             # TODO: longest test
             # TODO: shortest test
             # TODO: test per autor
-            # TODO:
+            #
     return out
 
-header = dict(zip(keys, header))
+# print(data, type(data))
 
-print(performances)
+# stats_keys = ['Avg days per test', 'Longest test', 'Shortest test', 'Total tests']
+# stats= [{}]
+stats_headers = ['Tests by author', 'Total days per tests', 'Avg days per test']
+stats_data = []
+# for i in range(len(list(authors))):
+#     stats_data += 1
+#     stats[a]['Total days per tests'] = 0
+#     for idx, row in enumerate(data):
+#         keys = ('Title', 'Assignee', 'PR', 'Finished On', 'Days per test')
+#         stats[a]['Tests by author'] +=1
+#         stats[a]['Total days per tests'] += row['Days per test']
+#         # if stats['Days per test'] > ['Title']
+#
+#     stats[a]['Avg days per test'] = stats[a]['Total days per tests'] / stats[a]['Tests by author']
+#
+# print(stats)
+
 data = add_weeks(data)
 print(data)
 
